39c1a9ad6412600a485a6d63a091323d3ebdd475
TyuioPi <j17tan@gmail.com>
2018-09-24 01:56:58 +1000

    Added display route functionality

--- app/src/main/java/com/s3628594/controller/DisplayRouteInfo.java
+++ app/src/main/java/com/s3628594/controller/DisplayRouteInfo.java
@@ -1,7 +1,6 @@
 package com.s3628594.controller;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +9,14 @@ import android.widget.AdapterView;
 import com.s3628594.model.Tracking;
 import com.s3628594.model.TrackingService;
 import com.s3628594.view.MapsActivity;
+
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class DisplayRouteInfo implements AdapterView.OnItemClickListener {
 
@@ -24,12 +31,52 @@ public class DisplayRouteInfo implements AdapterView.OnItemClickListener {
 
         final String LOG_TAG = DisplayRouteInfo.class.getName();
         Tracking tracking = (Tracking) adapterView.getAdapter().getItem(i);
+        ArrayList<String> routeInfoList = new ArrayList<>();
 
+        // Get route information of clicked tracking
         for (TrackingService.TrackingInfo routeInfo : tracking.getRouteInfo()) {
             Log.i(LOG_TAG, routeInfo.toString());
+            Date fixedRouteDate = correctDateFormat(routeInfo.date);
+            String route = String.format("%s,%s,%s,%s",fixedRouteDate, routeInfo.latitude, routeInfo.longitude, routeInfo.stopTime);
+            routeInfoList.add(route);
         }
 
         Intent intent = new Intent(context, MapsActivity.class);
+
+        // Pass data through intent
+        intent.putStringArrayListExtra("routeInfoList", routeInfoList);
+
         context.startActivity(intent);
+    }
+
+    private Date correctDateFormat(Date date) {
+        DateFormat dayFormat = new SimpleDateFormat("dd", Locale.getDefault());
+        DateFormat monthFormat = new SimpleDateFormat("MM", Locale.getDefault());
+        DateFormat temp = new SimpleDateFormat("MMM dd HH:mm:ss zzz yyyy", Locale.getDefault());
+
+        // Switch day and month to correct format
+        String day = monthFormat.format(date);
+        String month = dayFormat.format(date);
+
+        // Convert month int to string name
+        DateFormatSymbols dfs = new DateFormatSymbols();
+        String[] months = dfs.getMonths();
+        String fixedMonth = months[Integer.parseInt(month) - 1];
+
+        // Correct the day and month
+        StringBuilder stringBuilder = new StringBuilder(date.toString());
+        stringBuilder.replace(4,7, fixedMonth.substring(0,3));
+        stringBuilder.replace(8,10, day);
+
+        // Correct the date
+        String tempDate = stringBuilder.substring(4);
+        Date fixedDate = null;
+
+        try {
+            fixedDate = temp.parse(tempDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return fixedDate;
     }
 }
--- app/src/main/java/com/s3628594/view/MapsActivity.java
+++ app/src/main/java/com/s3628594/view/MapsActivity.java
@@ -1,6 +1,8 @@
 package com.s3628594.view;
 
 import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,7 +16,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.s3628594.geotracking.R;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -30,6 +41,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=enc:_kjwFjtsbMt%60EgnKcqLcaOzkGari%40naPxhVg%7CJjjb%40cqLcaOzkGari%40naPxhV:&key=YOUR_API_KEY
     */
     private GoogleMap mMap;
+    private Polyline polyline;
+    private PolylineOptions polylineOptions = new PolylineOptions();
+    private Date currentTime = Calendar.getInstance().getTime();
+    private Date trackingTime;
+    private Boolean trackingExist = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,10 +88,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        setMapPoints();
+        polyline = mMap.addPolyline(polylineOptions);
+        polyline.setWidth(3);
+        polyline.setColor(Color.BLUE);
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void setMapPoints() {
+        LatLng point = null, previousPoint;
+
+        // Retrieve passed data through intent
+        Intent intent = getIntent();
+        ArrayList<String> routeInfoList = intent.getStringArrayListExtra("routeInfoList");
+
+        for (int i = 0; i < routeInfoList.size(); i++) {
+            String[] route = routeInfoList.get(i).split(",");
+            Date previousTrackingTime = trackingTime;
+            trackingTime = convertStringToDate(route[0]);
+            Double latitude = Double.parseDouble(route[1]);
+            Double longitude = Double.parseDouble(route[2]);
+            int stopTime = Integer.parseInt(route[3]);
+            previousPoint = point;
+            point = new LatLng(latitude, longitude);
+            polylineOptions.add(point);
+
+            if (i == routeInfoList.size() - 1) {
+                setMeetLocTracking(point, trackingTime, stopTime);
+            } else {
+                setCurrLocTracking(previousPoint, point, previousTrackingTime, trackingTime);
+            }
+        }
+    }
+
+    private void setCurrLocTracking(LatLng previousPoint, LatLng newPoint, Date previousTrackingTime, Date trackingTime) {
+        if (previousPoint != null && newPoint != null) {
+            if (currentTime.after(previousTrackingTime) && currentTime.before(trackingTime)) {
+                mMap.addMarker(new MarkerOptions().position(previousPoint).title("Current Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(previousPoint, 16));
+                trackingExist = true;
+            } else if (currentTime.compareTo(previousTrackingTime) == 0) {
+                mMap.addMarker(new MarkerOptions().position(previousPoint).title("Current Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(previousPoint, 16));
+                trackingExist = true;
+            } else if (currentTime.compareTo(trackingTime) == 0) {
+                mMap.addMarker(new MarkerOptions().position(newPoint).title("Current Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPoint, 16));
+                trackingExist = true;
+            }
+        } else if (previousPoint == null && newPoint != null) {
+            if (currentTime.compareTo(trackingTime) == 0){
+                mMap.addMarker(new MarkerOptions().position(newPoint).title("Current Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPoint, 16));
+                trackingExist = true;
+            }
+        }
+    }
+
+    private void setMeetLocTracking(LatLng newPoint, Date trackingTime, int stopTime) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(trackingTime);
+        calendar.add(Calendar.MINUTE, stopTime);
+        Date trackingEndTime = calendar.getTime();
+
+        if (currentTime.after(trackingTime) && currentTime.before(trackingEndTime)) {
+            mMap.addMarker(new MarkerOptions().position(newPoint).title("Tracking At Meet Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPoint, 16));
+        } else {
+            mMap.addMarker(new MarkerOptions().position(newPoint).title("Meet Location"));
+            if (trackingExist == false) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPoint, 16));
+            }
+        }
+    }
+
+    private Date convertStringToDate(String date) {
+        Date convertedDate = null;
+        DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+
+        try {
+            convertedDate = dateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return convertedDate;
     }
 }
